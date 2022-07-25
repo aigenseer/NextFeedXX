ab82c4c384ff5b3ec208263eb531c7c38291f1bc
aigenseer <viktor.aigenseer@web.de>
2022-07-25 17:26:44 +0200

    Add default config

---
+++ src/main/java/com/nextfeed/NextFeedApplication.java
@@ -0,0 +1,13 @@
+package com.nextfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextfeed/NextfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
