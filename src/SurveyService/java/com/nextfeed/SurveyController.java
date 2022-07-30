0fc8be4f0ee6097827c494644047cc443111ebf7
aigenseer <viktor.aigenseer@web.de>
2022-07-30 19:30:35 +0200

    set json MediaType

--- src/SurveyService/java/com/nextfeed/SurveyController.java
+++ src/SurveyService/java/com/nextfeed/SurveyController.java
@@ -2,16 +2,19 @@ package com.nextfeed;
 
 import com.nextfeed.lib.TestLib;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/survey", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SurveyController {
 
     private final TestLib testLib;
 
-    @GetMapping("/survey/test")
+    @GetMapping("/test")
     public TestRequest create() {
         System.out.println("yes geht");
         return new TestRequest("Test");
